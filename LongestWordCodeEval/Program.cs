4a7a8e24c724a013dedf02e9d5306eeacad835c2
Stephen2Page <stephen2page@gmail.com>
2016-09-13 22:42:09 -0400

    final version

---
+++ LongestWordCodeEval/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWordCodeEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                        continue;
+                    {
+                        string wordLongest = "a";
+                        int wordLongestLength = 0;
+                        string[] words = line.Split(' ');
+
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            int wordLength = words[i].Length;  // Finds length of word
+                            if (wordLength > wordLongestLength)
+                            {
+                                wordLongestLength = wordLength;
+                                wordLongest = words[i];
+                            }
+                        }
+                        System.Console.WriteLine(wordLongest);
+                    }
+                }
+           
+        }
+    }
+}
